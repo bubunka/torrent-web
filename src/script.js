7a496b73af779c135ea259f08d6b9373f40d3ac0
bubunka <73910369+bubunka@users.noreply.github.com>
2020-11-04 08:00:18 +0700

    asd

--- src/script.js
+++ src/script.js
@@ -110,3 +110,28 @@ function byteSize(bytes, precision) {
 		return bytes + " B";
 	}
 }
+
+    <script>
+        window.webtor = window.webtor || [];
+        window.webtor.push({
+            id: 'player',
+            magnet: $scope.m.url,
+            on: function(e) {
+                if (e.name == window.webtor.TORRENT_FETCHED) {
+                    console.log('Torrent fetched!')
+                }
+                if (e.name == window.webtor.TORRENT_ERROR) {
+                    console.log('Torrent error!')
+                }
+            },
+            poster: 'https://via.placeholder.com/150/0000FF/808080?Text=Sintel',
+            subtitles: [
+                {
+                    srclang: 'en',
+                    label: 'test',
+                    src: 'https://raw.githubusercontent.com/andreyvit/subtitle-tools/master/sample.srt',
+                }
+            ],
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@webtor/player-sdk-js/dist/index.min.js" charset="utf-8"></script>
